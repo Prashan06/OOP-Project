658ca86c15d6cfce6c1c7256c5000f8f040cf30f
Prashan Anjanpalage <a1924253@adelaide.edu.au>
2024-09-16 12:21:06 +0930

    asa

---
+++ Farmer.h
@@ -0,0 +1,28 @@
+#ifndef FARMER_H
+#define FARMER_H
+
+#include <iostream>
+#include <string>
+
+class Farmer {
+    protected:
+    float Money;
+    Field Fields**;
+    Barn Barns**;
+    Tool Tools**;
+    int DayLength;
+    int DayCount;
+    std::string FarmName;
+
+    public:
+    float getMoneyCount();
+    void setMoneyCount();
+    void addFields();
+    void addBarns();
+    void addTools();
+    void getStatus();
+    void setFarmName();
+    std::string getFarmName();
+};
+
+#endif
