06351650f1e3801bdce096d3747e5e8c6fa9b623
a1925263 <a1925263@adelaide.edu.au>
2024-09-16 13:09:59 +0930

    D

--- Farmer.cpp
+++ Farmer.cpp
@@ -24,16 +24,21 @@ void Farmer:: getStatus(){
 
 
 void Farmer::setFarmName() {
-    cout << "what would you like to call your farm (max 20 characters): " << endl;
-    cin >> FarmName;
-    int i = 0;
-    while (FarmName[i] != '\0'){
-        i++;
+    bool validName = true;
+    while (validName) {
+        cout << "what would you like to call your farm (max 20 characters): " << endl;
+        cin >> FarmName;
+        int i = 0;
+        while (FarmName[i] != '\0'){
+            i++;
+        }
+        if (i > 20){
+            cout << "Farm name is too long, enter a shorter one" << endl;
+        } else {
+            validName = false;
+        }
+        
     }
-    if (i > 20){
-        cout << "Farm name is too long, enter a shorter one" << endl;
-    }
-
 }
 string Farmer::getFarmName() {
     return FarmName;
