3f6b1dab9d5c68968956d8a230e0746392351d12
a1925263 <a1925263@adelaide.edu.au>
2024-09-16 13:37:32 +0930

    creating farm abstract class

---
+++ Farm.h
@@ -0,0 +1,16 @@
+#ifndef FARMER_H
+#define FARMER_H
+
+#include <iostream>
+
+class Farm {
+
+    public:
+        virtual int sellItem() = 0;
+        virtual double setGrowthRate() = 0;
+        virtual void setPrice() = 0;
+        virtual void getPrice() = 0;
+        virtual void getNumberOfItems() = 0; 
+};
+
+#endif
