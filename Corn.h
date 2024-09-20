5ecb05ce4df852f0460fefe6b64ae61920122b00
a1925263 <a1925263@adelaide.edu.au>
2024-09-20 14:20:34 +0930

    writing corn.h and corn.cpp files

---
+++ Corn.h
@@ -0,0 +1,37 @@
+#ifndef CORN_H
+#define CORN_H
+
+#include "Barn.h"
+#include "ctime"
+
+// This class will contains methods relating to the breeding, selling ,etc of pigs.
+// This class inherits from the Barn class.
+class Corn : public Barn {
+    protected:
+    // The attributes of the function are initialised. 
+        int maxCornCapacity;
+        int cornGrowthRate;
+        int cornCount;
+        int cornPrice; 
+        int boughtCorn;
+        int numberOfTimesCornIsBought;  
+        int* cornArray = new int [numberOfTimesCornIsBought];
+        int* timeArray = new int [numberOfTimesCornIsBought];
+        int increaseCapacityPrice;
+        int sellReadyCornCount;
+        int sellReadyCornPrice;
+        int sellPrice;
+        int newMoney;
+    
+
+    public:
+        Corn();
+        int setMaxCropCapacity(int maxCornCapacity);
+        void increaseFieldCapacity();
+        int getCornCount();
+        void sellItem();
+        void buyItem();
+        ~Corn();
+};
+
+#endif
