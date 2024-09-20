01c28fcd626b1262e3f5133e357096d13cb60fb3
a1925263 <a1925263@adelaide.edu.au>
2024-09-20 14:39:31 +0930

    writing events

---
+++ Events.h
@@ -0,0 +1,18 @@
+#ifndef EVENTS.H
+#define EVENTS.H
+#include <iostream>
+#include "Farmer.h"
+#include "Pigs.h"
+#include "Cows.h"
+#include "Corn.h"
+
+using namespace std;
+
+class Events : public Farmer , public Pigs {
+
+};
+
+
+
+
+#endif
