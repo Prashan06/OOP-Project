8622b9ee7d700e50b788cd73322675c8b6b54bb3
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-09-28 12:53:35 +0930

    basic read capability to mainGame

--- mainGame.cpp
+++ mainGame.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include "progress.txt" // needs editing
 
 #include "Farm.h"
 #include "Farmer.h"
@@ -13,5 +15,24 @@ using namespace std;
 int main(){
 
 //Logic for mainGame
+
+Farmer player;
+
+ifstream inputFile("progress.txt");
+
+    if (!inputFile.is_open()) {
+        cout << "Error opening the file!" << endl;
+    }
+
+    string stringMoney;
+
+    while (getline(inputFile, stringMoney)) {
+        cout << "Your have " << stringMoney << " in the bank" << endl;
+        
+        int newMoney = player.getMoneyCount() + stoi(stringMoney);
+        player.setMoneyCount(newMoney);
+    }
+
+    inputFile.close(); 
 
 };
