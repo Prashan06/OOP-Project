8b0f500cb35d4e21fbb2932116c479a21f66d5f8
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-09-28 12:39:32 +0930

    Creating mainGame.cpp

---
+++ mainGame.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#include "Farm.h"
+#include "Farmer.h"
+#include "Corn.h"
+#include "Wheat.h"
+#include "Pigs.h"
+#include "Cows.h"
+
+using namespace std;
+
+int main(){
+
+//Logic for mainGame
+
+};
