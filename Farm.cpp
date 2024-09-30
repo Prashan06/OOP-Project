7571861f4670b6568c0d0764af2b3ad332a77eaf
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-09-30 23:03:16 +0930

    transferring prashan's changes to Farm to main

---
+++ Farm.cpp
@@ -0,0 +1,23 @@
+#include "Farm.h"
+
+Farm::Farm() : buyPrice(0), sellPrice(0), sellTime(60), timer(NULL){}
+void Farm::applySpeedGrow(){}
+void Farm::applyHighYield(){}
+int Farm::setBuyPrice(int buyPrice){}
+int Farm::getBuyPrice(){
+    return buyPrice;
+}
+void Farm::setSellPrice(int sellPrice){
+    this -> sellPrice = sellPrice;
+}
+int Farm::getSellPrice(){
+    return sellPrice;
+}
+void Farm::setSellTime(int sellTime){
+    this -> sellTime = sellTime;
+}
+int Farm::getSellTime(){
+    return sellTime;
+}
+
+Farm::~Farm(){}
