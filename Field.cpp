654dfb7491e3aeeb147d09579d014a5a666c8225
Prashan Anjanpalage <a1924253@adelaide.edu.au>
2024-10-01 21:30:07 +0930

    Changes to Field

--- Field.cpp
+++ Field.cpp
@@ -4,10 +4,48 @@ Field::Field(){
 
 }
 
+void Field::setSpeedGrowPrice (int speedGrowPrice){
+    this -> speedGrowPrice = speedGrowPrice;
+}
+int Field::getSpeedGrowPrice (){
+    return speedGrowPrice;
+}
+void Field::setPesticidePrice (int pesticidePrice){
+    this -> pesticidePrice = pesticidePrice;
+}
+int Field::getPesticidePrice (){
+    return pesticidePrice;
+}
+void Field::setHighYieldFactor(float highYieldFactor){
+    this -> highYieldFactor = highYieldFactor;
+}
+int Field::getHighYieldFactor(){
+    return highYieldFactor;
+}
+void Field::setHighYieldPrice(int highYieldPrice){
+    this -> highYieldPrice = highYieldPrice;
+}
+int Field::getHighYieldPrice(){
+    return highYieldPrice;
+}
+
+void Field::applySpeedGrowth(){
+    cout << "Do you want to buy the SpeedGrow potion? Y or N " << endl;
+    cin >> optionChoice;
+    if (optionChoice == "Y"){
+        
+    }
+}
+
+void applyPesticide(){
+    cout << "Do you want to buy pesticide? Y or N " << endl;
+    if (optionChoice == "Y" && pesticideApplied == false){
+        // Note for corn and wheat events, if pesticide applied is true, reduce killing of corn and wheat.
+        pesticideAppled = true;
+    }
+}
+
 Field::~Field(){
 
 }
 
-void Field::applySpeedGrowth(){
-    
-}
--- Field.h
+++ Field.h
@@ -2,18 +2,32 @@
 #define FIELD_H
 
 #include "Farm.h"
+#include <iostream>
+using namespace std;
 
 class Field: public Farm{
 
     protected:
+    int SpeedGrowLevel;
+	int speedGrowPrice;
+	int pesticidePrice;
+	bool pesticideApplied;
+	float highYieldFactor;
+	int highYieldPrice;
 
     public:
-
         Field();
+        void setSpeedGrowPrice (int speedGrowPrice);
+        int getSpeedGrowPrice ();
+        void setPesticidePrice (int pesticidePrice);
+        int getPesticidePrice ();
+        void setHighYieldFactor(float highYieldFactor);
+	    int getHighYieldFactor();
+        void setHighYieldPrice(int highYieldPrice);
+	    int getHighYieldPrice();
+        void applySpeedGrow();
+	    void applyPesticide();
 	    ~Field();
-
-        void applySpeedGrowth();
-
 };
 
 #endif
