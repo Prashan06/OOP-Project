b20fef9f91c8b158b48a024eb1e17d7cc0f56d8e
Prashan Anjanpalage <a1924253@adelaide.edu.au>
2024-10-01 15:15:44 +0930

    Changed Farm.cpp

--- Farm.cpp
+++ Farm.cpp
@@ -1,23 +1,39 @@
 #include "Farm.h"
 
+// Constructor
 Farm::Farm() : buyPrice(0), sellPrice(0), sellTime(60), timer(NULL){}
 void Farm::applySpeedGrow(){}
 void Farm::applyHighYield(){}
-int Farm::setBuyPrice(int buyPrice){}
+
+// Sets the buyPrice.
+int Farm::setBuyPrice(int buyPrice){
+    this -> buyPrice = buyPrice;
+}
+
+// Returns the buyPrice
 int Farm::getBuyPrice(){
     return buyPrice;
 }
+
+// sets the sellPrice
 void Farm::setSellPrice(int sellPrice){
     this -> sellPrice = sellPrice;
 }
+
+// returns the sellPrice
 int Farm::getSellPrice(){
     return sellPrice;
 }
+
+// sets the sellTime
 void Farm::setSellTime(int sellTime){
     this -> sellTime = sellTime;
 }
+
+// returns the sellTime.
 int Farm::getSellTime(){
     return sellTime;
 }
 
+// Destructor.
 Farm::~Farm(){}
