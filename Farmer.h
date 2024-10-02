2df9fa5a755870bd951059984995c9a5a51918c0
Prashan Anjanpalage <a1924253@adelaide.edu.au>
2024-10-02 14:52:25 +0930

    Farmer BuyItem

--- Farmer.h
+++ Farmer.h
@@ -12,53 +12,38 @@
 #include "Farm.h"
 
 class Farmer {
-
     protected:
-        
-        int money;
-
-        //vector arrays
-        vector<Wheat> wheatArray
-        vector<Corn> cornArray
-        vector<Pig> pigArray
-        vector<Cow> cowArray
-
-        //tracking variables for object creation
-        int timesWheatPlanted;
-        int timesCornPlanted;
-        int timesCowsBought;
-        int timesPigsBought;
-
-        char farmName[21];
-        int cowCount;
-        int pigCount;
-        int wheatCount;
-        int cornCount;
-   
+	int money;
+	Wheat** wheat;
+	Corn** corn;
+	Pig** pig;
+	Cow** cow;
+	char farmName[21];
+	int cowCount;
+	int pigCount;
+	int wheatCount;
+	int cornCount;
+	Farm farm;
     public:
-
-        Farmer();
-        ~Farmer();
-
-        //getters & setters
-        int getMoneyCount();
-        void setMoneyCount(int money);
-        string getFarmName();
-        Void setFarmName();
-        Void getStatus();
-        Void buyItem();
-        Int getCropCount ();
-        Int getAnimalCount ();
-        void sellItem();
-        int getCowCount()
-        int getPigCount();
-        int getWheatCount();
-        int getCornCount();
-        Void setCowCount(int cowCount);
-        Void setPigCount(int pigCount);
-        Void setWheatCount(int wheatCount);
-        Void setCornCount(int cornCount);
+    Farmer();
+	int getMoneyCount();
+	void setMoneyCount(int money);
+	string getFarmName();
+	Void setFarmName();
+	Void getStatus();
+	Void buyItem();
+    Int getCropCount ();
+    Int getAnimalCount ();
+	void sellItem();
+	int getCowCount()
+	int getPigCount();
+	int getWheatCount();
+	int getCornCount();
+	Void setCowCount(int cowCount);
+	Void setPigCount(int pigCount);
+	Void setWheatCount(int wheatCount);
+	Void setCornCount(int cornCount);
+	~Farmer();
 
 };
-
 #endif
