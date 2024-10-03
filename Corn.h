2d67bf28420d4da55f0be71576da285aa745ef53
Prashan Anjanpalage <a1924253@adelaide.edu.au>
2024-10-03 12:21:36 +0930

    Cow changes

---
+++ Corn.h
@@ -0,0 +1,8 @@
+#ifndef CORN_H
+#define CORN_H
+
+class Corn : public Field {
+
+};
+
+#endif
--- Cow.h
+++ Cow.h
@@ -9,7 +9,10 @@ class Cow: public Barn, public Events{
     protected:
 
     public:
-
+    Cows() - Constructor
+    ~Cows()
+Void applyHighYield() 
+Void events
         Cow();
         void applyHighYield();
 
