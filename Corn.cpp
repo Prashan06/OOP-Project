652514726adc5d1effacb8580dd862caf4f2a2f9
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-10-04 13:48:33 +0930

    updating events function for corn

--- Corn.cpp
+++ Corn.cpp
@@ -1,34 +1,33 @@
 #include "Corn.h"
 
-Corn::Corn() : Field() {
-    this -> sellPrice = 15;
-    this -> buyPrice = 10;
-    this -> sellTime = 35;
+Corn::Corn():Field(){
+
+    this->buyPrice = 20;
+    this->sellPrice = 30;
+    this->sellTime = 40;
+
+    this->pesticidePrice = 2;
+	this->highYieldFactor = 1.2;
+    this->highYieldPrice = 2;
+
+}
+
+void Corn::Event(Farmer ourFarmer){
+    cout << "A swarm of locusts have attacked you fields" << endl;
+    int newCornCount = ourFarmer.getCornCount() * 0.5;
+    for (int i = newCornCount; i < ourFarmer.getCornCount(); i++){
+        ourFarmer.getCornArray()[i - newCornCount] = ourFarmer.getCornArray()[i];
+    }
+    ourFarmer.setCornCount(newCornCount);
 }
 
 void Corn::applyHighYield(Farmer ourFarmer){
+
     if (ourFarmer.getMoneyCount() < highYieldPrice){
         cout << "You do not have enough money to buy high yield fertilizer" << endl;
     }else{
         int newMoney = ourFarmer.getMoneyCount() - this->highYieldPrice;
         ourFarmer.setMoneyCount(newMoney);
     }
-    this -> sellPrice = sellPrice * getHighYieldFactor();
-}
 
-
-void Corn::Event(Farmer ourFarmer){ 
-    if (getPesticideApplied() == true){
-        cout << "A disease has spread throughout the corn, as you have pesticide, your corn growth rate has increased by 10 seconds" << endl;
-        for (int i = 0; i < ourFarmer.getCornCount(); i++){
-            ourFarmer.getCornArray()[i]->sellTime= ourFarmer.getCornArray()[i]->sellTime + 10; 
-        }
-    }
-
-    if (getPesticideApplied() == false){
-        cout << "A disease has spread throughout the corn, as you do not have pesticide, your corn growth rate has increased by 20 seconds" << endl;
-        for (int i = 0; i < ourFarmer.getCornCount(); i++){
-            ourFarmer.getCornArray()[i]->sellTime= ourFarmer.getCornArray()[i]->sellTime + 20; 
-        }
-    }
 }
