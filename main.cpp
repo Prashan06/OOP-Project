4e49f60d06222d2d85947b8478244030ed67acb9
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-10-04 13:45:58 +0930

    creating main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+#include "Farmer.h"
+#include "Farm.h"
+#include "Field.h"
+#include "Barn.h"
+#include "Cow.h"
+#include "Pig.h"
+#include "Wheat.h"
+#include "Corn.h"
+#include "Events.h"
+
+using namespace std;
+
+int main(){
+
+    Farmer ourFarmer;
+
+}
