1fb238b04d744a5400ce3e73bb543a68ff53d1fb
Prashan Anjanpalage <a1924253@adelaide.edu.au>
2024-10-04 09:56:59 +0930

    Corn event and Pig event changes

--- Pig.cpp
+++ Pig.cpp
@@ -16,11 +16,11 @@ void Pig::applyHighYield( Farmer ourFarmer){
     this -> sellPrice = sellPrice*highYieldFactor;
 }
 
-void Pig::Event(Farmer farmer){
+void Pig::Event(Farmer ourFarmer){
     cout << "the demand for pig meat greatly decreased, in order to maintain a healthy flow of pigs " << endl;
     cout << "coming in and out of the farm, the sell price of all currently owned pigs must be reduced by half." << endl;
-    for (int i = 0; i < farmer.getPigCount(); i++){
-        farmer.getPigArray()[i]->sellPrice == (farmer.getPigArray()[i]->sellPrice) * 0.5;
+    for (int i = 0; i < ourFarmer.getPigCount(); i++){
+        ourFarmer.getPigArray()[i]->sellPrice == (ourFarmer.getPigArray()[i]->sellPrice) * 0.5;
     }
 }
 
