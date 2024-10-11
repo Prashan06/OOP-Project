304f6220746d5980531e34486f06fd5e021fb601
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-10-11 11:01:50 +1030

    commenting and debugging

--- Corn.cpp
+++ Corn.cpp
@@ -1,8 +1,8 @@
 #include <random>
 #include "Corn.h"
 
+Corn::Corn():Field(){ //constructor
 
-Corn::Corn():Field(){
     this->buyPrice = 20;
     this->sellPrice = 30;
     this->sellTime = 5;
@@ -11,7 +11,9 @@ Corn::Corn():Field(){
 }
 
 void Corn::Event(int* count){
-    if (*count % 10 == 0) {
+
+    if (*count % 10 == 0){
+
         random_device rd; // obtain a random number from hardware
         mt19937 gen(rd()); // seed the generator, initialise generator
         uniform_int_distribution<> distr(1, 10); // define the range
@@ -20,24 +22,34 @@ void Corn::Event(int* count){
         if (randomNumber == 1) {
             cornEvent = true;
         }
+
     }
 }
 
 void Corn::applyHighYield(int* money){
 
     if (*money < highYieldPrice){
-        cout << "You do not have enough money to buy high yield fertilizer" << endl;
-    } else {
+
+        cout << "You do not have enough money to buy high yield fertilizer" << endl; //checks whether the player has enough money to execute the function
+
+    } else{
+
         *money = (*money) - this->highYieldPrice;
         cornHighYieldApplied = true;
         cout << "highYield successfully applied" << endl;
+
     }
 
 }
 
-void Corn::setCornEvent(bool cornEvent) {
+void Corn::setCornEvent(bool cornEvent){ //setter for cornEvent
+
     this -> cornEvent = cornEvent;
+
 }
-bool Corn::getCornEvent() {
+
+bool Corn::getCornEvent(){ //getter for cornEvent
+
     return cornEvent;
+
 }
--- Corn.h
+++ Corn.h
@@ -2,22 +2,20 @@
 #define CORN_H
 
 #include "Field.h"
-#include "Events.h"
-#include "Wheat.h"
-#include "Cow.h"
-#include "Pig.h"
 
-
-
-class Corn: public Field, public Events {
+class Corn: public Field, public Events{
 
     protected:
-         bool cornEvent;
+
+        bool cornEvent;
+
     public:
         
-        Corn();
-        void Event(int* count);
-        void applyHighYield(int* money);
+        Corn(); //constructor 
+        void Event(int* count); 
+        void applyHighYield(int* money); //modifies sellPrice & money (in farmer)
+
+        //getters and setters fo cornEvent
         void setCornEvent(bool cornEvent);
         bool getCornEvent();
 
