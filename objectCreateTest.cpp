17d5ca6ea7542ea171610074e4fdbc05b7792b21
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-10-11 11:56:17 +1030

    testing logic for applyHighYield

--- objectCreateTest.cpp
+++ objectCreateTest.cpp
@@ -1,24 +1,32 @@
 void Farmer::upgradeCrop(){
-    cout << "What crop would you like to upgrade, wheat or corn? (Enter response in lower case)" << endl;
+
+    string product;
+
+    cout << "What crop would you like to upgrade? wheat or corn? (Enter response in lower case)" << endl;
     cin >> product;
-    while (product != "wheat" && product != "corn") {
+
+    while (product != "wheat" && product != "corn"){
         cout << "invalid input! please enter wheat or corn" << endl;
         cin >> product;
     }
 
-    if (product == "wheat"){
-        string optionChoice;
-        time_t currentTime = time(0); 
-        int j = 0;
-        int highYieldNotAppliedCount = 0;
+    if (product == "wheat"){ //
+
+        int option;
         int newMoney = 0;
+        int highYieldNotAppliedCount;
+
         for (int i = 0; i < wheatCount; ++i) {
-            if (wheat[i]->getIsHighYieldApplied() == false) {
+            if ((wheat[i]->getIsHighYieldApplied()) == false){
                 newMoney = newMoney + wheat[i]->getHighYieldPrice();
                 highYieldNotAppliedCount++;
             }
         }
-        cout << "you have " << highYieldNotApplied << " wheat that are not upgraded " << newMoney <<" Would you like to upgrade them them (enter Y or N)" << endl;
+
+        int numberCanUpgrade = money%wheat[0]->getHighYieldPrice
+
+        cout << "you have " << highYieldNotApplied << " wheat that are not upgraded " << endl;
+        cout << "You can upgrade" << numberCanUpgrade << "of them, enter the number that you would like to upgrade or 0 to exit" << endl;
         cin >> optionChoice;
         while (optionChoice != "Y" && optionChoice != "N") {
             cout << "invalid input! please enter Y or N" << endl;
@@ -26,6 +34,7 @@ void Farmer::upgradeCrop(){
         }
         if (optionChoice == "Y") {
             for (int i = 0; i < wheatCount; ++i) {
+                if (j < optionChoice){}
                 if ((currentTime - (wheat[i]->getTimer())) < wheat[i]->getSellTime()){
                     newWheatArray[j] = wheat[i];
                     j++;
@@ -36,37 +45,8 @@ void Farmer::upgradeCrop(){
             money = money + newMoney;
             wheat = newWheatArray;
             setWheatCount(wheatCount - readyToSellCount);
-        }
+
+    }else if (product == "corn"){
+    }//
+} //
         
-    } else if (getProduct() == "corn") {
-        string optionChoice;
-        time_t currentTime = time(0); 
-        int j = 0;
-        int readyToSellCount = 0;
-        int newMoney = 0;
-        for (int i = 0; i < cornCount; ++i) {
-            cout << "Yo" << endl;
-            if (corn[i]->isReadyToSell() == true){
-            newMoney = newMoney + corn[i]->getSellPrice();
-            readyToSellCount++;
-            }
-        }
-        Corn** newCornArray = new Corn*[cornCount - readyToSellCount];
-        cout << "you have " << readyToSellCount << " corn to sell for " << newMoney <<" Would you like to sell them (enter Y or N)" << endl;
-        cin >> optionChoice;
-        while (optionChoice != "Y" && optionChoice != "N") {
-            cout << "invalid input! please enter Y or N" << endl;
-            cin >> optionChoice;
-        }
-        if (optionChoice == "Y") {
-            for (int i = 0; i < cornCount-1; ++i) {
-                if (corn[i]->isReadyToSell() == false){
-                newCornArray[j++] = corn[i];
-                }
-            }
-            money = money + newMoney;
-            corn = newCornArray;
-            setCornCount(cornCount - readyToSellCount);
-        }
-    }
-}
