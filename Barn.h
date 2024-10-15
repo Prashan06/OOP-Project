5cccf33e223633a089dcabec1177a2a455a98820
190022student <dilmika.jayawardena@student.adelaide.edu.au>
2024-10-15 14:29:42 +1030

    formatting

--- Barn.h
+++ Barn.h
@@ -3,33 +3,29 @@
 
 #include "Farm.h"
 
+// This class has functions and attributes that manage the barn. This class
+// inherits from Farm and is inherited by the cow and pig classes.
+class Barn : public Farm {
+ protected:
+  int speedGrowPrice;
+  float highYieldFactor;
+  int highYieldPrice;
+  int speedGrowLevel;
+  bool speedGrowApplied;
 
-// This class has functions and attributes that manage the barn. This class inherits from Farm and is inherited by the cow and pig classes.
-class Barn: public Farm {
-
-    protected:
-
-        int speedGrowPrice;
-	    float highYieldFactor;
-	    int highYieldPrice;
-	    int speedGrowLevel;
-        bool speedGrowApplied;
-
-    public:
-
-        Barn(); // Constructor 
-        void setHighYieldFactor(float highYieldFactor);
-        float getHighYieldFactor(); 
-        void setHighYieldPrice(int highYieldPrice); 
-        int getHighYieldPrice();
-        void setSpeedGrowPrice (int speedGrowPrice);
-        int getSpeedGrowPrice ();
-        void applySpeedGrowth(int* money);
-        void setSpeedGrowLevel (int speedGrowLevel);
-        int getSpeedGrowLevel();
-        void setSpeedGrowApplied(bool speedGrowApplied);
-        bool getSpeedGrowApplied();
-    
+ public:
+  Barn();  // Constructor
+  void setHighYieldFactor(float highYieldFactor);
+  float getHighYieldFactor();
+  void setHighYieldPrice(int highYieldPrice);
+  int getHighYieldPrice();
+  void setSpeedGrowPrice(int speedGrowPrice);
+  int getSpeedGrowPrice();
+  void applySpeedGrowth(int* money);
+  void setSpeedGrowLevel(int speedGrowLevel);
+  int getSpeedGrowLevel();
+  void setSpeedGrowApplied(bool speedGrowApplied);
+  bool getSpeedGrowApplied();
 };
 
 #endif
--- Farmer.h
+++ Farmer.h
@@ -1,182 +1,88 @@
-/*#ifndef FARMER_H
-#define FARMER_H
-
-#include <string>
-#include <ctime>
-#include <chrono>
-#include <fstream>
-
-#include "Wheat.h"
-#include "Corn.h"
-#include "Cow.h"
-#include "Pig.h"
-#include "Farm.h"
-#include "Barn.h"
-#include "Field.h"
-
-using namespace std;
-
-class Farmer: public Pig, public Corn, public Cow, public Wheat {
-    protected:
-        
-        int money;
-        int cowCount = 0;
-        int pigCount = 0;
-        int wheatCount = 0;
-        int cornCount = 0;
-        //arrays
-        Wheat** wheat;
-        Corn** corn;
-        Pig** pig;
-        Cow** cow;
-
-        //tracking variables for object creation
-        char farmName[21];
-		int timesWheatPlanted;
-        int timesCornPlanted;
-        int timesCowsBought;
-        int timesPigsBought;
-        string product;
-        fstream outputFile;
-        fstream inputFile;
-   
-    public:
-
-        Farmer();
-        ~Farmer();
-
-        //getters & setters
-        int* getMoneyCount();
-        void setMoneyCount(int money);
-        string getFarmName();
-        void setFarmName();
-        void getStatus();
-        int* getCowCount();
-        int* getPigCount();
-        int* getWheatCount();
-        int* getCornCount();
-        void setCowCount(int cowCount);
-        void setPigCount(int pigCount);
-        void setWheatCount(int wheatCount);
-        void setCornCount(int cornCount);
-        void buyAnimal();
-        void buyCrop();
-        void sellAnimal();
-        void sellCrop();
-        void fieldSpeedGrowthBought();
-        void barnSpeedGrowthBought();
-        void highYieldBought();
-        void executeEvent();
-        string getProduct();
-        void setProduct(string product);
-
-		Cow** getCowArray();
-		Pig** getPigArray();
-		Wheat** getWheatArray();
-		Corn** getCornArray();
-		Cow* createNewCow();
-		Pig* createNewPig();
-		Wheat* createNewWheat();
-		Corn* createNewCorn();
-
-        void fileWriter(string filename, int* money);
-        void fileReader(string filename);
-
-
-};
-#endif
-
-*/
-
 #ifndef FARMER_H
 #define FARMER_H
 
-#include <string>
-#include <ctime>
 #include <chrono>
+#include <ctime>
 #include <fstream>
+#include <string>
 
-#include "Wheat.h"
+#include "Barn.h"
 #include "Corn.h"
 #include "Cow.h"
-#include "Pig.h"
 #include "Farm.h"
-#include "Barn.h"
 #include "Field.h"
+#include "Pig.h"
+#include "Wheat.h"
 
 using namespace std;
 
-class Farmer: public Pig, public Corn, public Cow, public Wheat {
-    protected:
-        
-        int money;
-        int cowCount = 0;
-        int pigCount = 0;
-        int wheatCount = 0;
-        int cornCount = 0;
-        //arrays
-        Wheat** wheat;
-        Corn** corn;
-        Pig** pig;
-        Cow** cow;
+class Farmer : public Pig, public Corn, public Cow, public Wheat {
+ protected:
+  int money;
+  int cowCount = 0;
+  int pigCount = 0;
+  int wheatCount = 0;
+  int cornCount = 0;
+  // arrays
+  Wheat** wheat;
+  Corn** corn;
+  Pig** pig;
+  Cow** cow;
 
-        //tracking variables for object creation
-        char farmName[21];
-		int timesWheatPlanted;
-        int timesCornPlanted;
-        int timesCowsBought;
-        int timesPigsBought;
-        string product;
-        fstream outputFile;
-        fstream inputFile;
-   
-    public:
+  // tracking variables for object creation
+  char farmName[21];
+  int timesWheatPlanted;
+  int timesCornPlanted;
+  int timesCowsBought;
+  int timesPigsBought;
+  string product;
+  fstream outputFile;
+  fstream inputFile;
 
-        Farmer();
-        ~Farmer();
+ public:
+  Farmer();
+  ~Farmer();
 
-        //getters & setters
-        int* getMoneyCount();
-        void setMoneyCount(int money);
-        string getFarmName();
-        void setFarmName();
-        void getStatus();
-        int* getCowCount();
-        int* getPigCount();
-        int* getWheatCount();
-        int* getCornCount();
-        void setCowCount(int cowCount);
-        void setPigCount(int pigCount);
-        void setWheatCount(int wheatCount);
-        void setCornCount(int cornCount);
-        void executeEvent();
-        void buyAnimal();
-        void buyCrop();
-        void sellAnimal();
-        void sellCrop();
-        //void fieldSpeedGrowthBought();
-        void applySpeedGrowCrops();
-        //void barnSpeedGrowthBought();
-        void applySpeedGrowAnimals();
-        //void highYieldBought();
-        void applyHighYieldAnimals();
-        void applyHighYieldCrops();
-        string getProduct();
-        void setProduct(string product);
-        void appliedPesticide();
+  // getters & setters
+  int* getMoneyCount();
+  void setMoneyCount(int money);
+  string getFarmName();
+  void setFarmName();
+  void getStatus();
+  int* getCowCount();
+  int* getPigCount();
+  int* getWheatCount();
+  int* getCornCount();
+  void setCowCount(int cowCount);
+  void setPigCount(int pigCount);
+  void setWheatCount(int wheatCount);
+  void setCornCount(int cornCount);
+  void executeEvent();
+  void buyAnimal();
+  void buyCrop();
+  void sellAnimal();
+  void sellCrop();
+  // void fieldSpeedGrowthBought();
+  void applySpeedGrowCrops();
+  // void barnSpeedGrowthBought();
+  void applySpeedGrowAnimals();
+  // void highYieldBought();
+  void applyHighYieldAnimals();
+  void applyHighYieldCrops();
+  string getProduct();
+  void setProduct(string product);
+  void appliedPesticide();
 
-		Cow** getCowArray();
-		Pig** getPigArray();
-		Wheat** getWheatArray();
-		Corn** getCornArray();
-		Cow* createNewCow();
-		Pig* createNewPig();
-		Wheat* createNewWheat();
-		Corn* createNewCorn();
+  Cow** getCowArray();
+  Pig** getPigArray();
+  Wheat** getWheatArray();
+  Corn** getCornArray();
+  Cow* createNewCow();
+  Pig* createNewPig();
+  Wheat* createNewWheat();
+  Corn* createNewCorn();
 
-        void fileWriter(string filename, int* money);
-        void fileReader(string filename);
-
-
+  void fileWriter(string filename, int* money);
+  void fileReader(string filename);
 };
 #endif
